1d3e086533636d468e56418f288ea33184d10990
BOHDANff <popivbod12@gmail.com>
2022-04-08 18:28:33 +0300

    Develop form for adding items

--- front/src/components/Form.js
+++ front/src/components/Form.js
@@ -1,49 +1,93 @@
 import React from 'react';
 import * as yup from "yup";
-import {useForm} from "react-hook-form";
+import {useForm, Controller} from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup/dist/yup";
 import {MyForm} from "./UI/MyForm/MyForm";
 import {MyInput} from "./UI/MyInput";
 import {MyFormButton} from "./UI/MyFormButton";
+import {MyRadio} from "./UI/MyRadio";
+import FormLabel from "@mui/material/FormLabel";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Radio from "@mui/material/Radio";
+import FormControl from "@mui/material/FormControl";
 
 function Form(props) {
     const schema = yup.object().shape({
-        title: yup
+        name: yup
             .string()
-            .required("This is a required field"),
-        body: yup
+            .required("This is a required field")
+            .max(20, `Must be less than 20 characters`),
+        phone: yup
             .string()
-            .required("This is a required field"),
+            .required("This is a required field")
+            .matches(/^([+]?[0-9]{10,14})?$/, "Write your phone number correctly"),
+        age: yup
+            .number()
+            .positive('Write your age correctly')
+            .max(120, 'Please enter your real age')
+            .nullable(true)
+            .transform((v) => (v === '' || Number.isNaN(v) ? null : v)),
+        gender: yup
+            .boolean(),
     });
-    const {register, handleSubmit, formState:{errors}, reset} = useForm({
+    const {register, handleSubmit, formState:{errors}, reset, control} = useForm({
         mode: "onChange",
         resolver: yupResolver(schema),
+        defaultValues: {
+            gender: null,
+        },
     })
-    const onSubmit = (notat) => {
-        // dispatch(createItem({...notat, id: String(Date.now())}))
-        // reset()
-        // props.setVisible(false)
+    const onSubmit = (item) => {
+        if (!item.age) {
+            delete item.age
+        }
+        if (item.gender === null) {
+            delete item.gender
+        }
+        console.log(item);
+        reset()
     }
-    // const dispatch = useDispatch()
     return (
         <MyForm onSubmit={handleSubmit(onSubmit)}>
             <MyInput
-                {...register('title')}
-                id={'title'}
+                {...register('name')}
+                id={'name'}
                 type={'text'}
-                label={'Label of notation'}
-                error={!!errors.title}
-                helperText={errors?.title?.message}
+                label={'Your name'}
+                error={!!errors.name}
+                helperText={errors?.name?.message}
             />
             <MyInput
-                {...register('body')}
-                id="body"
+                {...register('phone')}
+                id="phone"
                 type="text"
-                label="Text of notation"
-                multiline
-                rows={4}
-                error={!!errors.body}
-                helperText={errors?.body?.message}
+                label="Your phone"
+                error={!!errors.phone}
+                helperText={errors?.phone?.message}
+            />
+            <FormControl>
+                <FormLabel>Gender</FormLabel>
+                <Controller
+                    control={control}
+                    name="gender"
+                    render={({field}) => {
+                    return (
+                    <RadioGroup {...field} row>
+                        <FormControlLabel  control={<Radio />} value={false} label="Female"/>
+                        <FormControlLabel  control={<Radio />} value={true} label="Male"/>
+                    </RadioGroup>
+                    )
+                }}
+                />
+                </FormControl>
+            <MyInput
+                {...register('age')}
+                id="age"
+                type="number"
+                label="Your age"
+                error={!!errors.age}
+                helperText={errors?.age?.message}
             />
             <MyFormButton style={{marginTop:10, width: "40%"}}>Create item</MyFormButton>
         </MyForm>
--- front/src/components/Item.js
+++ front/src/components/Item.js
@@ -3,12 +3,28 @@ import {Card, CardActions, CardContent, Typography} from "@mui/material";
 import Button from "@material-ui/core/Button";
 
 const Item = (props) => {
+    console.log(props.gender);
     return (
-        <Card sx={{minWidth: 275}} style={{marginBottom: "20px"}} variant="outlined">
+        <Card sx={{minWidth: '100%'}} style={{marginBottom: "20px"}} variant="outlined">
             <CardContent>
-                <Typography variant="h5" component="div">
-                    {props.title}
+                <Typography variant="h6" component="div">
+                    Name: {props.name}
                 </Typography>
+                <Typography variant="h6" component="div">
+                    Phone: {props.phone}
+                </Typography>
+                { props.gender === undefined ?
+                    <></> :
+                    <Typography variant="h6" component="div">
+                        Gender: {props.gender}
+                    </Typography>
+                }
+                {!props.age?
+                        <></> :
+                        <Typography variant="h6" component="div">
+                            Age: {props.age}
+                        </Typography>
+                }
             </CardContent>
             <CardActions>
                 <Button size="small" >Delete</Button>
